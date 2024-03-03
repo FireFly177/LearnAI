39301da06a788e463eb057666597ccff60e5fdd0
Duy Nguyen <huonghaiduynhim@gmail.com>
2024-03-03 12:35:03 +0700

    Add nnfs and spiral_data imports, update layer dimensions, and add activation function

--- neuralnet.py
+++ neuralnet.py
@@ -1,4 +1,6 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
 np.random.seed(0)
 class Layer_Dense:
@@ -17,11 +19,10 @@ X = [   [1, 2, 3, 2.5],
         [2.0, 5.0, -1.0, 2.0],
         [-1.5, 2.7, 3.3, -0.8]  ]
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+X, y = spiral_data(samples=100, classes=3)
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
 
 layer1.forward(X)
-# print(layer1.weights)
-# print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+activation1.forward(layer1.output)
